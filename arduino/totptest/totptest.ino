450ef11ef3c7f3ca64911bf1f3da8e9c8f702eb8
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-01-19 08:48:22 +0800

    made some changes

--- arduino/totptest/totptest.ino
+++ arduino/totptest/totptest.ino
@@ -1,55 +1,68 @@
 #include <RTClib.h>
 #include <Wire.h>
 #include <TOTP.h>
+#include <rgb_lcd.h>
 
 // The shared secret is ML9pIK9ImW
-uint8_t hmacKey[] = {0x4a, 0x56, 0x47, 0x44, 0x53, 0x34, 0x43, 0x4a, 0x4a, 0x4d, 0x34, 0x55, 0x53, 0x33, 0x4b, 0x58};
-TOTP totp = TOTP(hmacKey, 16);
+uint8_t hmacKey[] = {0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x77, 0x6f, 0x72, 0x6c, 0x64};
+TOTP totp = TOTP(hmacKey, 10);
 RTC_DS1307 RTC;
+rgb_lcd lcd;
 
-void setup() {
+const int colorR = 0;
+const int colorG = 0;
+const int colorB = 255;
+
+void setup()
+{
+
+  // Configure LCD
+  lcd.begin(16,2);  
+  lcd.setRGB(colorR, colorG, colorB);
 
   // Begin serial
   Serial.begin(9600);
-  
-  // Begin RTC & calibrate if need be
+
+  // Begin RTC
   Wire.begin();
   RTC.begin();
 
-  if (!RTC.isrunning()) {
-    Serial.println("RTC is NOT running.");
-  }
+  // if (!RTC.isrunning()) {
+  //   Serial.println("RTC is NOT running.");
+  // }
 
-  DateTime now = RTC.now();
-  DateTime system = DateTime(__DATE__, __TIME__);
+//  DateTime now = RTC.now();
+  // DateTime system = DateTime(__DATE__, __TIME__);
 
-  if (now.unixtime() < system.unixtime()) {
-    Serial.println("Updating RTC clock with latest time...");
-    RTC.adjust(DateTime(__DATE__, __TIME__));
-  }
+  // if (now.unixtime() < system.unixtime()) {
+  //   Serial.println("Updating RTC clock with latest time...");
+  RTC.adjust(DateTime(2020, 1, 18, 21, 33, 20));
+  // }
 }
 
-void loop() {
-
+void loop()
+{
   DateTime now = RTC.now();
-  
-  Serial.print("Current time: ");
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  
-  char* newCode = totp.getCode(now.unixtime());
-  Serial.println(newCode);
 
-  delay(5000);
+  lcd.print(now.day(), DEC);
+  lcd.print('/');
+  lcd.print(now.month(), DEC);
+  lcd.print('/');
+  lcd.print(now.year(), DEC);
+  lcd.print(" ");
+
+  lcd.print(now.hour(), DEC);
+  lcd.print(':');
+  lcd.print(now.minute(), DEC);
+
+  lcd.setCursor(0,1);
+
+  char *newCode = totp.getCode(now.unixtime());
+  lcd.print("PIN:");
+  lcd.print(newCode);
+
+   delay(1000);
+  
+  lcd.clear();
 
 }
